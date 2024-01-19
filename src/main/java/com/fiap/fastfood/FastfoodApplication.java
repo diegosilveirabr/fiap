d5ba3c913deb33326e0669eb580c54908f8639c9
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-01-19 20:55:26 -0300

    chore: ajuste de title e description no swagger + ajustes no readme

--- src/main/java/com/fiap/fastfood/FastfoodApplication.java
+++ src/main/java/com/fiap/fastfood/FastfoodApplication.java
@@ -1,11 +1,14 @@
 package com.fiap.fastfood;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableMongoAuditing
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Fast Food FIAP", description = "Projeto de um sistema para lanchonete realizado para a Fase 1 da Pós-Graduação de Arquitetura de Sistemas da FIAP", version = "v1"))
 public class FastfoodApplication {
 
     public static void main(String[] args) {
